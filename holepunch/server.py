c03413ff356f9e267df4d35453147174c30436fa
Emmanouil Matsis <emmanouilmatsis@gmail.com>
2015-01-13 13:29:05 +0000

    modified:   holepunch/server.py new file:   holepunch/thread.py modified:   holepunch/worker.py

--- holepunch/server.py
+++ holepunch/server.py
@@ -6,53 +6,6 @@ import holepunch.config
 import holepunch.thread
 
 
-class Server:
-
-    def __init__(self):
-        self._conn = None
-        self._addr = (holepunch.config.HOST, holepunch.config.PORT)
-
-        self._clients = []
-
-        self._lock = threading.Lock()
-        self._tasks = queue.Queue()
-        self._workers = [Worker(self, self._tasks) for _ in range(holepunch.config.WORKERS_SIZE)]
-
-    def _open(self):
-        print("opening...")
-        self._conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self._conn.bind(self._addr)
-        self._conn.listen(5)
-
-        for worker in self._workers:
-            worker.start()
-
-    def _close(self):
-        print("closing...")
-        self._conn.shutdown(socket.SHUT_RDWR)
-        self._conn.close()
-
-        for worker in self._workers:
-            worker.stop()
-
-    def run(self):
-        self._open()
-
-        while True:
-            try:
-                conn, addr = self._conn.accept()
-
-                client = holepunch.client.Client(self._server, conn, addr)
-                self._clients.append(client)
-
-                task = holepunch.thread.Task(self._lock, client)
-                self._tasks.put(task)
-            except KeyboardInterrupt:
-                break
-
-        self._close()
 
 
 class Server:
--- holepunch/worker.py
+++ holepunch/worker.py
@@ -1,86 +1,26 @@
 import sockets
 import threading
 
+import holepunch.config
 import holepunch.request
 
 
-class Worker(threading.Thread):
-
-    def __init__(self, lock, tasks):
-        threading.Thread.__init__(self)
-        self._tasks = tasks
-
-    def start(self):
-        threading.Thread.start(self)
-        self._running = True
-
-    def stop(self):
-        self._running = False
-
-    def run(self):
-        while self._running:
-            task = self._tasks.get()
-            while not task.done:
-                task.execute()
-            break
-
-
-class Task:
-
-    @property
-    def done(self):
-        return self._done
-
-    @done.setter
-    def done(self, value):
-        self._done = value
-
-    def __init__(self, lock):
-        self._lock = lock
-        self._done = False
-
-    def execute(self):
-        raise NotImplementedError
-
-
-class ClientTask(holepunch.Task):
-
-    def __init__(self, lock, client):
-        super().__init__(lock)
-        self._client = client
-
-    def _parse(self, data):
-        return self.holepunch.request.Request(data)
-
-    def _handle(self, data):
-        request = self._parse(data)
-
-    def execute(self):
-        data = self._client.recv()
-        if not data:
-            self._client.close()
-            self._complete = True
-
-        request = self._parse(data)
-        self._handle(request)
-
-
-class Client:
-
-    def __init__(self, server, conn, addr):
-        self._server = server
-        self._conn = conn
-        self._addr = addr
-
-    def open(self):
-        pass
-
-    def close(self):
-        self._conn.shutdown(socket.SHUT_RDWR)
-        self._conn.close()
-
-    def recv(self):
-        return self._conn.recv(1024)
-
-    def send(self, data):
-        self._conn.sendall(data)
+#class Client:
+#
+#    def __init__(self, server, conn, addr):
+#        self._server = server
+#        self._conn = conn
+#        self._addr = addr
+#
+#    def open(self):
+#        pass
+#
+#    def close(self):
+#        self._conn.shutdown(socket.SHUT_RDWR)
+#        self._conn.close()
+#
+#    def recv(self):
+#        return self._conn.recv(1024)
+#
+#    def send(self, data):
+#        self._conn.sendall(data)
