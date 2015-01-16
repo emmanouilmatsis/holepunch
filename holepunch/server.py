b32cd23abdce7fa1513f4757704d239948e27004
Emmanouil Matsis <emmanouilmatsis@gmail.com>
2015-01-16 18:07:02 +0000

    modified:   holepunch/server.py

--- holepunch/server.py
+++ holepunch/server.py
@@ -11,32 +11,26 @@ class Server:
         self._sock = None
         self._addr = (holepunch.config.HOST, holepunch.config.PORT)
 
-        self._rlist = []
+        self._rlist = [self]
         self._wlist = []
         self._xlist = []
 
-        self._clients = []
-
     def fileno(self):
-        return self._sock
+        return self._sock.fileno()
 
     def open(self):
+        logging.info("Open server%s", self._addr)
+
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self._sock.bind(self._addr)
         self._sock.listen(5)
 
-        self._rlist.append(self._sock)
-
-        logging.info("Open server%s.", self._addr)
-
     def close(self):
+        logging.info("Close server%s", self._addr)
+
         self._sock.close()
-
-        self._rlist.remove(self._sock)
-
-        logging.info("Close server%s.", self._addr)
 
     def run(self):
         self.open()
@@ -45,36 +39,33 @@ class Server:
             try:
                 rready, wready, xready = select.select(self._rlist, self._wlist, self._xlist)
 
-                for socket in rready:
-                    if socket == self._sock:
-                        sock, addr = socket.accept()
-
-                        client = Client(self, sock, addr)
-                        client.open
+                for r in rready:
+                    if r is self:
+                        r.accept_client()
                     else:
-                        client = self.find_client(sock=socket)
-                        client.handle()
+                        r.handle()
 
             except KeyboardInterrupt:
-                for client in self._clients:
-                    client.close()
                 break
 
         self.close()
 
+    def accept_client(self):
+        sock, addr = self._sock.accept()
+        client = Client(self, sock, addr)
+        client.open()
+
     def find_client(self, sock=None, addr=None):
-        for client in self._clients:
-            if client.sock == sock or client.addr == addr:
-                return client
+        for r in self._self._rlist:
+            if r.sock == sock or r.addr == addr:
+                return r
         return None
 
     def append_client(self, client):
-        self._clients.append(client)
-        self._rlist.append(client.sock)
+        self._rlist.append(client)
 
     def remove_client(self, client):
-        self._clients.remove(client)
-        self._rlist.remove(client.sock)
+        self._rlist.remove(client)
 
 
 class Client:
@@ -101,46 +92,68 @@ class Client:
         self._addr = addr
 
     def fileno(self):
-        return self._sock
+        return self._sock.fileno()
 
     def open(self):
+        logging.info("Open client%s", self._addr)
+
         self._server.append_client(self)
 
-        logging.info("Open client%s.", self._addr)
-
     def close(self):
-        self._sock.close()
-        self._server.remove_client(self)
+        logging.info("Close client%s", self._addr)
 
-        logging.info("Close client%s.", self._addr)
+        self._server.remove_client(self)
 
     def recv(self):
         data = self._sock.recv(holepunch.config.BUFSIZE)
-        return data
+        return Message(data)
 
-    def send(self, data):
-        self._sock.sendall(data)
+    def send(self, message):
+        self._sock.sendall(message)
 
     def handle(self):
+        logging.info("Handle client%s", self._addr)
+
         data = self.recv()
 
         if not data:
             self.close()
 
-        logging.info("Handle client%s.", self._addr)
 
+class Message:
 
-class Request:
+    @property
+    def method(self):
+        return self._method
+
+    @method.setter
+    def method(self, value):
+        self._method = value
+
+    @property
+    def body(self):
+        return self._body
+
+    @body.setter
+    def body(self, value):
+        self._body = value
 
     def __init__(self, data):
-        self._data = data
         self._method = None
         self._body = None
 
+        self.parse(data)
 
-class Response:
+    def __bytes__(self):
+        pass
 
-    def __init__(self, data):
-        self._data = data
-        self._method = None
-        self._body = None
+    def validate(self, data):
+        isMethod = data[0] in [">", "<", "!", "?"]
+        isBody
+
+    def parse(self, data):
+        method = data[0]
+        body = data[1:]
+
+        if method in [">", "<", "!", "?"]:
+            if method == ">":
